81eae6285b7c66e6198b778c84353688c7e4b290
Kseniya Kochava <kseniya_kochava@epam.com>
2022-04-27 00:28:49 +0300

    ‘task5fixed2’

--- task5fixed/Feed.py
+++ task5fixed/Feed.py
@@ -2,30 +2,44 @@ from datetime import date, datetime
 
 
 class Feed:
-    def createfeed(var):
-        pass
+    def __init__(self):
+        self.today = date.today()
+
+    def input_text(self):
+        text = input("Please enter your text: ")
+        return text
+
+    def input_city(self):
+        city = input("Please enter your city: ")
+        return city
+
+    def input_date(self):
+        expiration_date = input("Please enter your ad's expiration date in YYYY-MM-DD format: ")
+        return expiration_date
+
+    def diff_count(self, expiration):
+        date_object = datetime.strptime(expiration, '%Y-%m-%d').date()
+        diff = date_object - self.today
+        diff = diff.days
+        return diff
+
 
 class News(Feed):
-    def createfeed(var):
-        text = input("Please enter your news' text: ")
-        city = input("Please enter your city: ")
-        news = str("News:" + '\n' + text + '\n' + city + ', ' + str(date.today()) + '\n' + '\n')
+    def createfeed(self):
+        news = str("News:" + '\n' + str(self.input_text()) + '\n' + str(self.input_city()) + ', ' + str(self.today) + '\n' + '\n')
         return news
 
+
 class Ad(Feed):
-    def createfeed(var):
-        text_ad = input("Please enter your text_ad: ")
-        expiration_date = input("Please enter your ad's expiration date in YYYY-MM-DD format: ")
-        today = date.today()
-        date_object = datetime.strptime(expiration_date, '%Y-%m-%d').date()
-        diff = date_object - today
-        ad = str("Ad:" + '\n' + text_ad + '\n' + 'Actual until: ' + str(date_object) + ', ' + (
-                    str(diff.days) + ' day(s) left.') + '\n' + '\n')
+    def createfeed(self):
+        expiration_date = self.input_date()
+        ad = str("Ad:" + '\n' + str(self.input_text()) + '\n' + 'Actual until: ' + str(expiration_date) + ', ' + (
+                    str(self.diff_count(expiration_date)) + ' day(s) left.') + '\n' + '\n')
         return ad
 
+
 class Forecast(Feed):
-    def createfeed(var):
-        location = input("Please enter your location: ")
+    def createfeed(self):
         text = 'The weather forecast is not available for selected region.'
-        forecast = str("Forecast:" + '\n' + text + '\n' + location + ', ' + str(date.today()) + '\n' + '\n')
+        forecast = str("Forecast:" + '\n' + text + '\n' + str(self.input_city()) + ', ' + str(self.today) + '\n' + '\n')
         return forecast
